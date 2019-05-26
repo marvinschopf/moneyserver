3920eab9541886f79ceffeaf919088d9cfd5e802
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2019-05-26 13:12:59 +0200

    add some testing lines

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const config = require('./config');
 const express = require('express')
 import JsonDB from 'node-json-db';
-import { Config } from 'node-json-db/lib/JsonDBConfig';
-const app = express()
-const port = 3000
+import { Config } from './node_modules/node-json-db/dist/lib/JsonDBConfig';
+const app = express();
+const port = 3000;
 
-const db = new JsonDB(new Config("database", true, false, '/');
+const db = new JsonDB(new Config("database", true, false, '/'));
 db.push("/test1","super test");
+console.log("Pushed!");
+db.save();
+db.reload();
 
 
 app.set('json spaces', 40);
