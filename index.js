85a96358205726a3e09361134de6c4f7c6e4dbc0
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2019-05-26 10:31:36 +0200

    add basic express construct

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+const config = require('./config');
 const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('json spaces', 40);
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.all('/api/v1/get-balance', (req, res) => {
+	config.keys.forEach((val) => {
+		if(val.key == req.query.key) {
+			res.json({"response": "success"});
+		} else {
+			res.json({"response": "error", "type": "noperm", "message": "You don't have permission to access this endpoint."});
+		}
+	});
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
